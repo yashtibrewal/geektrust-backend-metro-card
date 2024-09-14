35940326bbccb983316e92f9b96bd772073c0c1a
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-09-14 20:22:31 +0530

    Environment config

---
+++ src/geektrust.ts
@@ -0,0 +1,12 @@
+// import fs from 'fs';
+
+// const filename = process.argv[2]
+
+// fs.readFile(filename, "utf8", (err: NodeJS.ErrnoException | null, data: string) => {
+//     /*if (err) throw err
+//     var inputLines = data.toString().split("\n")
+//     // Add your code here to process input commands
+//     */
+// });
+
+console.log('Started');
